23ace0900370a20d6ca77148adefb218e15cbd25
nrowe <114745324+n-rowe@users.noreply.github.com>
2025-02-20 18:35:29 +0000

    ci: add coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     dir: './tests',
+    coverage: {
+      provider: 'v8',
+    },
     typecheck: {
       include: ['*.test.ts'],
     },
